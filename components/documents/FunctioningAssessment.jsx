6a0d7f54bd7de892f011e7ff756a2c5fa355b46b
user <user@MacBookAir.netis.cc>
2026-09-26 12:44:06 +0300

    fix: restore respiratory assessment accordions

--- components/documents/FunctioningAssessment.jsx
+++ components/documents/FunctioningAssessment.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { functioningCategories, respiratoryAssessmentSections } from '../../data/functioning/respiratoryAssessment';
 
 function ToolCard({ tool, onOpenQuestionnaire }) {
